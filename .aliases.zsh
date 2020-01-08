a1edc641a2c5a63cdc57a0798b41aee0bae85427
Tobias Fuchs <fuchstobs@gmail.com>
2020-01-09 00:51:15 +0100

    updates aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,20 +1,3 @@
-alias ls='lsd'
-alias la='lsd -a'
-alias xrandr-home-setup='echo "Switching on DP2" && xrandr --output DP2 --auto && xrandr --output DP2 --right-of eDP1'
-alias xrandr-not-home-setup='echo "Switching off monitor DP2" && xrandr --output DP2 --off'
-alias g='nvim --remote-silent'
-alias vpn='sudo vpnc --pid-file /var/run/vpnc/pid'
-alias lock='xscreensaver-command -lock'
-alias susp='systemctl suspend'
-alias tum_vpnc='sudo vpnc --pid-file /var/run/vpnc/pid'
-alias vpn-disconnect='sudo killall vpnc'
-alias music='spotify'
-alias l.='ls -d .* --color=auto'
-alias l='lsd -la'
-alias ping='ping -c 3'
-alias meminfo='free -m -l -t'
-alias matlab="matlab -nodesktop"
-
 #some quickfixes
 alias k="exit"
 
