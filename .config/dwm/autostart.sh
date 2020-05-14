15b32ada3f7114cb22fae47ec9c9529b3945e0e7
Tobias Fuchs <fuchstobs@gmail.com>
2020-05-15 01:38:44 +0200

    rips xmonad conf from DTs config

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -2,6 +2,9 @@
 
 nm-applet & 
 volumeicon & 
+clipit &
 setxkbmap -option "caps:swapescape" &
 xrandr --output DP-2 --left-of DP-4 &
 feh --bg-scale ~/Pictures/wallpaper/scourgebringer.jpg & 
+pcloud &
+keepassxc &
